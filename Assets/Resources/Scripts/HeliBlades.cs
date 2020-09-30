314d2c51ce67b677b3fc8f6fd63aeccc39b743d3
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2020-09-30 11:17:52 -0500

    Initial commit of complete project

---
+++ Assets/Resources/Scripts/HeliBlades.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeliBlades : MonoBehaviour
+{
+    public Vector3 rotation;
+    public float speed;
+
+    void Update()
+    {
+        transform.Rotate(rotation * (speed * Time.deltaTime));
+    }
+}
